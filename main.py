35d3f3fcf4d21ff49a99aa76e1e35d65644a2a3c
draymone <socotcho@gmail.com>
2024-05-02 18:13:02 +0200

    Add player movement

--- main.py
+++ main.py
@@ -4,11 +4,31 @@ import pyxel
 class Player:
     def __init__(self):
         self.SIZE = 10
+        self.SPEED = 2
+
         self.x = 30
         self.y = 30
 
     def update(self):
-        self.x+=1
+        self.handle_movement()
+
+    def handle_movement(self):
+        x, y = 0, 0
+
+        if pyxel.btn(pyxel.KEY_Z):
+            y -= 1
+        elif pyxel.btn(pyxel.KEY_S):
+            y += 1
+        if pyxel.btn(pyxel.KEY_D):
+            x += 1
+        elif pyxel.btn(pyxel.KEY_Q):
+            x -= 1
+
+        self.x += x * self.SPEED
+        self.y += y * self.SPEED
+
+    def handle_attack(self):
+        
 
     def draw(self):
         pyxel.cls(0)
@@ -24,7 +44,7 @@ class Game:
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_Q):
+        if pyxel.btn(pyxel.KEY_ESCAPE):
             pyxel.quit()
         self.player.update()
 
